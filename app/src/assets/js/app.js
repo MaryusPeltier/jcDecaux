c7544cc4066ba9c98085da8f1251bdeb44f92011
MaryusPeltier <47104640+MaryusPeltier@users.noreply.github.com>
2019-02-22 10:22:20 +0100

    sidenav modif

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -53,18 +53,12 @@ function formSubmitPopup(event){
         },
     })
 }
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-    document.getElementById("main").style.marginLeft = "300px";
-    document.getElementsByClassName("mapboxgl-popup-content").style.marginLeft = "250px";
-  }
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
-  
+
+$( "span" ).click(function() {
+    $( "#mySidenav" ).toggleClass( "moveSidenav" );
+    $("#main").toggleClass("moveMain");
+  });
+
 
 
 
